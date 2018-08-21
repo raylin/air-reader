463c1d090f215dbfe3d8c491b9d902567c323a86
Ray Lin <lieeray4136@gmail.com>
2018-08-22 00:33:43 +0800

    Record the data into sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 extern crate chrono;
 extern crate csv;
-extern crate serde;
-extern crate serialport;
-#[macro_use]
-extern crate serde_derive;
 extern crate rusqlite;
+extern crate serialport;
 
 use chrono::prelude::*;
+use chrono::NaiveDateTime;
+use rusqlite::Connection;
 use serialport::prelude::*;
 use std::io::{self, Write};
+use std::path::Path;
 use std::thread;
 use std::time::Duration;
 
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug)]
 struct Record {
-    local_time: DateTime<Local>,
+    local_time: NaiveDateTime,
     pm25: u16,        // ug/m3
     tvoc: u16,        // ppb
     hcho: u16,        // ug/m3
     co2: u16,         // ppm
-    temperature: f32, // ℃
-    humidity: f32,    // %
+    temperature: f64, // ℃
+    humidity: f64,    // %
 }
 
 fn process_bytes(bytes: &[u8; 24]) -> Record {
-    let local_time: DateTime<Local> = Local::now();
+    let local_time: NaiveDateTime = Local::now().naive_local();
+    //let local_time = NaiveDateTime::new();
     let pm25 = (bytes[4] as u16) * 0xff + (bytes[5] as u16);
     let tvoc = (bytes[6] as u16) * 0xff + (bytes[7] as u16);
     let hcho = (bytes[9] as u16) * 0xff + (bytes[10] as u16);
     let co2 = (bytes[12] as u16) * 0xff + (bytes[13] as u16);
-    let temperature = ((bytes[14] as f32) * 256.0 + (bytes[15] as f32)) / 10.0;
-    let humidity = ((bytes[16] as f32) * 256.0 + (bytes[17] as f32)) / 10.0;
+    let temperature = ((bytes[14] as f64) * 256.0 + (bytes[15] as f64)) / 10.0;
+    let humidity = ((bytes[16] as f64) * 256.0 + (bytes[17] as f64)) / 10.0;
 
     let record = Record {
         local_time,
@@ -48,6 +48,24 @@ fn process_bytes(bytes: &[u8; 24]) -> Record {
 }
 
 fn main() {
+    let path = Path::new("data.sqlite");
+    let conn = Connection::open(&path).unwrap();
+
+    conn.execute(
+        "CREATE TABLE air_quality 
+        (
+            id INTEGER PRIMARY KEY,
+            created_time DATETIME,
+            pm25 SMALLINT,
+            tvoc SMALLINT,
+            hcho SMALLINT,
+            co2 SMALLINT,
+            temperature FLOAT,
+            humidity FLOAT
+        )",
+        &[],
+    ).unwrap();
+
     let port_name = "/dev/tty.SLAB_USBtoUART";
     let baud_rate = 9600;
 
@@ -64,8 +82,6 @@ fn main() {
     let mut buf: [u8; 24] = [0; 24];
     let mut serial_buf: Vec<u8> = vec![0; 24];
     let mut cur = 0;
-
-    let mut wtr = csv::Writer::from_path("data.csv").expect("Failed to wrtie to file");
 
     println!(
         "Ready to receive data on {} at {} rate",
@@ -84,9 +100,27 @@ fn main() {
                     cur = 0;
                     continue;
                 } else if cur == (24 - 1) {
-                    wtr.serialize(process_bytes(&buf))
-                        .expect("Failed to write Record");
-                    wtr.flush().expect("Failed to flush writer");
+                    let record = process_bytes(&buf);
+                    conn.execute(
+                        "INSERT INTO air_quality (
+                        pm25,
+                        tvoc,
+                        hcho,
+                        co2,
+                        temperature,
+                        humidity,
+                        created_time
+                    ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+                        &[
+                            &record.pm25,
+                            &record.tvoc,
+                            &record.hcho,
+                            &record.co2,
+                            &record.temperature,
+                            &record.humidity,
+                            &record.local_time,
+                        ],
+                    ).unwrap();
                     cur = 0;
                 } else {
                     buf[cur] = *i;
